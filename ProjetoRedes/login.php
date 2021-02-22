bec5904cc0599775fefcac6b0c3b4dd3e0ea9515
RafaelPeixoto1 <72198789+RafaelPeixoto1@users.noreply.github.com>
2021-02-22 12:53:45 +0000

    Add files via upload

---
+++ ProjetoRedes/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="ISO-8859-1">
+	<title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+<form method="post" action="processa_login.php">
+	<label>Nome utilizador</label><input type="text" name="user_name"  required><br><br>
+	<label>Palavra-passe</label><input type="password" name="password"required><br><br>
+	<input type="submit" name="login">
+	<h1><p align="center">GINÁSIO</h1></p>
+<p align="center"><img  src="capturar.png"></p>
+</form>
+</body>
+</html>
